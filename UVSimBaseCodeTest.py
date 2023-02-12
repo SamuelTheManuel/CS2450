261f6105d0915aee8f58f1c3ef23e51ec5e89333
Sazzizzy <107164992+Sazzizzy@users.noreply.github.com>
2023-02-11 20:28:38 -0700

    Update UVSimBaseCodeTest.py

--- UVSimBaseCodeTest.py
+++ UVSimBaseCodeTest.py
@@ -8,6 +8,7 @@ import sys
 import mock
 from io import StringIO
 
+
 def test_all():
     '''
     tests all functions
@@ -22,10 +23,11 @@ def test_all():
     assert uvs.memory_dict["26"][1] == "-0442"
     assert uvs.accumulator[1] == "-0442"
 
+
 def test_load():
     '''load a word from a specific location in memory(val) into the accumulator'''
     sim = UVSim()
-    #basic test
+    # basic test
     sim.memory_dict["21"] = [True, "5005"]
     our_register = "21"
     sim.Load(sim.memory_dict[our_register])
@@ -41,22 +43,23 @@ def test_load():
 
 def test_store():
     '''store a word from the accumulator into a specific location(val) in memory'''
-    #basic test
+    # basic test
     sim = UVSim()
     sim.memory_dict["12"] = [True, "4321"]
     sim.accumulator = [True, "1234"]
     our_register = "12"
     sim.Store(our_register)
     assert sim.memory_dict["12"] != [True, "9999"]
-    
-    #test if given value is in memory
+
+    # test if given value is in memory
     sim = UVSim()
     sim.memory_dict["42"] = [True, "1221"]
     sim.accumulator = [True, "9999"]
     our_register = "99"
     sim.Store(our_register)
     assert sim.memory_dict["42"] != [True, "9999"]
-    
+
+
 def test_add1():
     sim = UVSim()
     # empty accumulator test
@@ -83,6 +86,7 @@ def test_add1():
     sim.Add(sim.memory_dict[our_register][1])
     assert sim.accumulator == [False, "0045"]
 
+
 def test_add2():
     sim = UVSim()
     # empty accumulator test
@@ -108,7 +112,7 @@ def test_add2():
     our_register = "60"
     sim.Add(sim.memory_dict[our_register][1])
     assert sim.accumulator == [False, "-0045"]
-    
+
 
 def test_subtract1():
     sim = UVSim()
@@ -128,6 +132,7 @@ def test_subtract1():
     sim.Subtract(sim.memory_dict[our_register][1])
     assert sim.accumulator == [False, "0545"]
 
+
 def test_subtract2():
     sim = UVSim()
     # empty register test
@@ -145,6 +150,7 @@ def test_subtract2():
     sim.memory_dict["65"] = [False, "-5545"]
     sim.Subtract(sim.memory_dict[our_register][1])
     assert sim.accumulator == [False, "6045"]
+
 
 def test_multiply1():
     sim = UVSim()
@@ -164,6 +170,7 @@ def test_multiply1():
     sim.Multiply(sim.memory_dict[our_register][1])
     assert sim.accumulator == [False, "-22500"]
 
+
 def test_multiply2():
     sim = UVSim()
     # empty register test
@@ -182,6 +189,7 @@ def test_multiply2():
     sim.Multiply(sim.memory_dict[our_register][1])
     assert sim.accumulator == [False, "-1120"]
 
+
 def test_divide1():
     sim = UVSim()
     # divide by zero test
@@ -196,6 +204,7 @@ def test_divide1():
     sim.memory_dict["50"] = [False, "0045"]
     sim.Divide(sim.memory_dict[our_register][1])
     assert sim.accumulator == [False, "0011"]
+
 
 def test_divide2():
     sim = UVSim()
@@ -212,26 +221,28 @@ def test_divide2():
     sim.Divide(sim.memory_dict[our_register][1])
     assert sim.accumulator == [False, "0005"]
 
+
 def test_Read():
-    #Testing that it works with a valid input
+    # Testing that it works with a valid input
     sim = UVSim()
     with mock.patch("builtins.input", return_value="1234"):
         result = sim.Read(0)
         assert result == [False, "1234"]
         assert sim.memory_dict[0] == [False, "1234"]
-    #Testing that it works with an invaild input
+    # Testing that it works with an invaild input
     with mock.patch("builtins.input", side_effect=["abcd", "5678"]):
         result = sim.Read(0)
         assert result == [False, "5678"]
         assert sim.memory_dict[0] == [False, "5678"]
-    #Testing what happens when the number does not have 4 digits
+    # Testing what happens when the number does not have 4 digits
     with mock.patch("builtins.input", side_effect=["123", "5678"]):
         result = sim.Read(0)
         assert result == [False, "5678"]
         assert sim.memory_dict[0] == [False, "5678"]
 
+
 def test_Write():
-    #Testing that it works on a vaild register
+    # Testing that it works on a vaild register
     sim = UVSim()
     sim.memory_dict = {0: [True, "1234"]}
     captured_output = StringIO()
@@ -240,15 +251,13 @@ def test_Write():
     sys.stdout = sys.__stdout__
     assert result == {'1234'}
     assert captured_output.getvalue() == "1234\n"
-    #Testing that it does not work with an invalid input
+    # Testing that it does not work with an invalid input
     captured_output = StringIO()
     sys.stdout = captured_output
     result = sim.Write(1)
     sys.stdout = sys.__stdout__
     assert result == None
     assert captured_output.getvalue() == ""
-
-
 
 
 def test_branch1(tmpdir):
@@ -262,7 +271,7 @@ def test_branch1(tmpdir):
 
     with b.open() as f:
         content = f.read()
-        #check for all the contents
+        # check for all the contents
         assert content == "+2003\n+2110\n+4010\n+3004\n+1200"
 
     uvs = UVSim()
@@ -270,7 +279,7 @@ def test_branch1(tmpdir):
     uvs.initiate_process(our_string)
 
     assert uvs.memory_dict["10"][1] == "3004"
-    assert uvs.accumulator == [False,"4204"]
+    assert uvs.accumulator == [False, "4204"]
 
 
 def test_branch2(tmpdir):
@@ -284,7 +293,7 @@ def test_branch2(tmpdir):
 
     with b.open() as f:
         content = f.read()
-        #check for all the contents
+        # check for all the contents
         assert content == "+2004\n+2150\n+4050\n+1200\n+3303"
 
     uvs = UVSim()
@@ -293,8 +302,7 @@ def test_branch2(tmpdir):
 
     print(uvs.memory_dict["50"])
     assert uvs.memory_dict["50"][1] == "3303"
-    assert uvs.accumulator == [False,"3963600"]
-
+    assert uvs.accumulator == [False, "3963600"]
 
 
 def test_branch_neg1(tmpdir):
@@ -309,10 +317,9 @@ def test_branch_neg1(tmpdir):
         f.write("+1234\n")
         f.write("+5678\n")
 
-
     with b.open() as f:
         content = f.read()
-        #check for all the contents
+        # check for all the contents
         assert content == "+2005\n+2160\n+2006\n+3107\n+4160\n+3000\n+1234\n+5678\n"
 
     uvs = UVSim()
@@ -336,10 +343,9 @@ def test_branch_neg2(tmpdir):
         f.write("+4300\n")
         f.write("+3000\n")
 
-
     with b.open() as f:
         content = f.read()
-        #check for all the contents
+        # check for all the contents
         assert content == "+2008\n+2170\n+2005\n+3106\n+4170\n+5678\n+1234\n+4300\n+3000\n"
 
     uvs = UVSim()
@@ -348,7 +354,6 @@ def test_branch_neg2(tmpdir):
 
     assert uvs.memory_dict["70"][1] == "3000"
     assert uvs.accumulator[1] == "4444"
-
 
 
 def test_branch_zero1(tmpdir):
@@ -363,10 +368,9 @@ def test_branch_zero1(tmpdir):
         f.write("+1234\n")
         f.write("+1234\n")
 
-
     with b.open() as f:
         content = f.read()
-        #check for all the contents
+        # check for all the contents
         assert content == "+2005\n+2160\n+2006\n+3107\n+4260\n+3000\n+1234\n+1234\n"
 
     uvs = UVSim()
@@ -390,10 +394,9 @@ def test_branch_zero1(tmpdir):
         f.write("+4300\n")
         f.write("+3000\n")
 
-
     with b.open() as f:
         content = f.read()
-        #check for all the contents
+        # check for all the contents
         assert content == "+2008\n+2170\n+2005\n+3106\n+4270\n+1234\n+1233\n+4300\n+3000\n"
 
     uvs = UVSim()
@@ -412,10 +415,9 @@ def test_branch_halt1(tmpdir):
         f.write("+2100\n")
         f.write("+0000\n")
 
-
     with b.open() as f:
         content = f.read()
-        #check for all the contents
+        # check for all the contents
         assert content == "+4300\n+3002\n+2100\n+0000\n"
 
     uvs = UVSim()
@@ -435,12 +437,10 @@ def test_branch_halt2(tmpdir):
         f.write("+1234\n")
         f.write("+5678\n")
 
-
     with b.open() as f:
         content = f.read()
-        #check for all the contents
+        # check for all the contents
         assert content == "+2004\n+4300\n+2005\n+1234\n+5678\n"
-        
 
     uvs = UVSim()
     our_string = content.strip().split()
