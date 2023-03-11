cc034bcafbbeca0ceb199f689220f7bec6264d29
Sazzizzy <107164992+Sazzizzy@users.noreply.github.com>
2023-03-11 15:58:12 -0700

    Update UVSimGUIColor.py

--- UVSimGUIColor.py
+++ UVSimGUIColor.py
@@ -7,59 +7,84 @@ class UVSimGUIColor:
         self.GUI = GUI
 
     def ChangePrimaryColor(self):
-        self.GUI.primary_color = colorchooser.askcolor(title="Choose Primary Color")[1]
-        self.GUI.primary_color_button.configure(background=self.GUI.primary_color)
-        self.UpdateColors()
+        primary_color = colorchooser.askcolor(title="Choose Primary Color")[1]
+        self.UpdateColors(primary=primary_color)
 
     def ChangeSecondaryColor(self):
-        self.GUI.secondary_color = colorchooser.askcolor(title="Choose Secondary Color")[1]
-        self.GUI.secondary_color_button.configure(background=self.GUI.secondary_color)
-        self.UpdateColors()
+        secondary_color = colorchooser.askcolor(title="Choose Secondary Color")[1]
+        self.UpdateColors(secondary=secondary_color)
 
     def ChangeTertiaryColor(self):
-        self.GUI.tertiary_color = colorchooser.askcolor(title="Choose Tertiary Color")[1]
-        self.GUI.tertiary_color_button.configure(background=self.GUI.tertiary_color)
-        self.UpdateColors()
+        tertiary_color = colorchooser.askcolor(title="Choose Tertiary Color")[1]
+        self.UpdateColors(tertiary=tertiary_color)
 
     def ChangeTextColor(self):
-        self.GUI.text_color = colorchooser.askcolor(title="Choose Text Color")[1]
-        self.GUI.text_color_button.configure(background=self.GUI.text_color)
-        self.UpdateColors()
+        text_color = colorchooser.askcolor(title="Choose Text Color")[1]
+        self.UpdateColors(textC=text_color)
 
     def ChangeSecondaryTextColor(self):
-        self.GUI.secondary_text_color = colorchooser.askcolor(title="Choose Secondary Text Color")[1]
-        self.GUI.secondary_text_color_button.configure(background=self.GUI.secondary_text_color)
-        self.UpdateColors()
+        secondary_text = colorchooser.askcolor(title="Choose Secondary Text Color")[1]
+        self.UpdateColors(secondaryText=secondary_text)
 
     def ChangeOutputBG(self):
-        self.GUI.output_bg = colorchooser.askcolor(title="Choose Secondary Text Color")[1]
-        self.GUI.output_color_button.configure(background=self.GUI.output_bg)
-        self.UpdateColors()
+        output_BG = colorchooser.askcolor(title="Choose Secondary Text Color")[1]
+        self.UpdateColors(outputBg=output_BG)
 
     def ChangeOutputText(self):
-        self.GUI.output_text_color = colorchooser.askcolor(title="Choose Secondary Text Color")[1]
-        self.GUI.secondary_text_color_button.configure(background=self.GUI.secondary_text_color)
-        self.UpdateColors()
+        output_text = colorchooser.askcolor(title="Choose Secondary Text Color")[1]
+        self.UpdateColors(outputText=output_text)
 
     def ResetColorsToDefault(self):
-        self.GUI.primary_color = self.GUI.default_primary_color
-        self.GUI.secondary_color = self.GUI.default_secondary_color
-        self.GUI.tertiary_color = self.GUI.default_tertiary_color
-        self.GUI.text_color = self.GUI.default_text_color
-        self.GUI.secondary_text_color = self.GUI.default_secondary_text_color
+        self.UpdateColors(primary=self.GUI.default_primary_color, secondary=self.GUI.default_secondary_color,
+                          tertiary=self.GUI.default_tertiary_color, textC=self.GUI.default_text_color,
+                          secondaryText=self.GUI.default_secondary_text_color,outputBg=self.GUI.default_output, outputText=self.GUI.default_output_text)
 
-    def UpdateColors(self):
-        self.GUI.style_Main.configure("Main Style", foreground=self.GUI.primary_color, background=self.GUI.text_color)
-        self.GUI.style_dom_button.configure("Main Style Dom Button", foreground=self.GUI.secondary_color, background=self.GUI.text_color)
-        self.GUI.style_sub_button.configure("Main Style Sub Button", foreground=self.GUI.tertiary_color, background=self.GUI.secondary_text_color)
-
-        our_guys = self.GUI.our_window.winfo_children()
+    def UpdateColors(self, primary=None, secondary=None, tertiary=None, textC=None, secondaryText=None, outputBg=None, outputText=None):
+        our_guys = self.GUI.style_dict
         for guy in our_guys:
-            if guy.cget("style") == "Main Style":
-                guy.configure(style="Main Style")
-            elif guy.cget("style") == "Main Style Dom Button":
-                guy.configure(style="Main Style Dom Button")
-            elif guy.cget("style") == "Main Style Sub Button":
-                guy.configure(style="Main Style Sub Button")
+            if self.CheckIfColor(primary):
+                self.SimplifySetColor(guy, primary, "Primary")
+            if self.CheckIfColor(secondary):
+                self.SimplifySetColor(guy, secondary, "Secondary")
+            if self.CheckIfColor(tertiary):
+                self.SimplifySetColor(guy, tertiary, "Tertiary")
+            if self.CheckIfColor(textC):
+                self.SimplifySetColor(guy, textC, "Text")
+            if self.CheckIfColor(secondaryText):
+                self.SimplifySetColor(guy, secondaryText, "SecondaryText")
+            if self.CheckIfColor(outputBg):
+                self.SimplifySetColor(guy, outputBg, "OutputBg")
+            if self.CheckIfColor(outputText):
+                self.SimplifySetColor(guy, outputText, "OutputText")
+        if self.CheckIfColor(primary):
+            self.GUI.our_window.configure(bg=primary)
+            self.GUI.primary_color = primary
+        if self.CheckIfColor(secondary):
+            self.GUI.secondary_color = secondary
+        if self.CheckIfColor(tertiary):
+            self.GUI.tertiary_color = tertiary
+        if self.CheckIfColor(textC):
+            self.GUI.our_window.configure(highlightcolor=textC)
+            self.GUI.text_color = textC
+        if self.CheckIfColor(secondaryText):
+            self.GUI.secondary_text_color = secondaryText
+        if self.CheckIfColor(outputBg):
+            self.GUI.output_bg = outputBg
+        if self.CheckIfColor(outputText):
+            self.GUI.output_text = outputText
+
+    def SimplifySetColor(self, guy, color, type_guy):
+        try:
+            if guy[1] == type_guy:
+                guy[0].configure(background=color)
+            if guy[2] == type_guy:
+                guy[0].configure(foreground=color)
+        except Exception:
+            pass
 
 
+    def CheckIfColor(self, our_color):
+        if our_color is None:
+            return False
+        else:
+            return True
